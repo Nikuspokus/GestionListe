5afe970c908716e4d1bbdabdd89cfffb40998124
marquillier <nicolas.marquillier@gmail.com>
2020-03-02 09:58:56 +0100

    first commit

---
+++ android/app/src/main/java/com/example/gestion_dune_liste/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gestion_dune_liste;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
